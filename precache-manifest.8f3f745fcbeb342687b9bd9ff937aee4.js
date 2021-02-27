e7870ef41860dc7b53930c922b7368cdc08939fe
Grzeisek Duliński <grzesiek746@gmail.com>
2021-02-27 10:06:05 +0100

    Updates

---
+++ precache-manifest.8f3f745fcbeb342687b9bd9ff937aee4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce0738653279eb1e50c0",
+    "url": "/ToDo-App/static/css/main.7f07d5d0.chunk.css"
+  },
+  {
+    "revision": "ce0738653279eb1e50c0",
+    "url": "/ToDo-App/static/js/main.8c458e5d.chunk.js"
+  },
+  {
+    "revision": "9957314125854f35cd44",
+    "url": "/ToDo-App/static/js/runtime~main.1ef025cf.js"
+  },
+  {
+    "revision": "d8ea4e540511c59ad1e5",
+    "url": "/ToDo-App/static/js/2.5442d947.chunk.js"
+  },
+  {
+    "revision": "a614c043bd17545da0444af0c65f4ae7",
+    "url": "/ToDo-App/index.html"
+  }
+];
